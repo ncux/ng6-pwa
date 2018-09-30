64f9f9e3ef381e9f27ae177246bcad60fd6d732e
Talent Malaba <malabatalent@gmail.com>
2018-09-30 11:56:36 +0800

    Building A PWA Using Angular 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Item } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements  OnInit {
+  title = 'PWA-Demo';
+  items:  Array<Item>;
+
+  constructor(private  apiService:  ApiService){   }
+
+  ngOnInit(){
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.apiService.fetch().subscribe((data: Array<Item>) => {
+      console.log(data);
+      this.items = data;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
+
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatCardModule } from  '@angular/material/card';
+import { MatButtonModule } from  '@angular/material/button';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
